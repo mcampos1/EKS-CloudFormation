137f1f5174e1d5d6d60c92e48983f4611d6776dd
markmccormack91 <mark.mccormack@mantech.com>
2024-03-18 13:52:37 -0600

    update region

--- private-eks/variables.sh
+++ private-eks/variables.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 CLUSTER_NAME='private-eks-cf-test'
-REGION=us-east-1
+REGION=us-gov-west-1
 HTTP_PROXY_ENDPOINT_SERVICE_NAME=""  # leave blank for no proxy, or populate with a VPC endpoint ID to create a PrivateLink powered connection to a proxy server
 KEY_PAIR="private-eks-cf-test-keypair"  # we need to manually create an EC2 key pair to be used as an SSH key on the worker nodes, put the name here
 VERSION='1.27'  # K8s version to deploy
