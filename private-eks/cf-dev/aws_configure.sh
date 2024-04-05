77893b8ad315f08ab1cace8e7e39869afa75e4ba
Martin Campos <martin.campos8@gmail.com>
2024-04-05 12:09:04 -0400

    bashscripts update

---
+++ private-eks/cf-dev/aws_configure.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+CLUSTER_NAME="private-eks"
+ACCESS_KEY=AKIA2MDVSHOEKCXZKR77
+SECRET_KEY=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
+INSTANCE_ID=i-048eaacbf7e9823aa
+INSTANCE_PROFILE=arn:aws-us-gov:iam::713211132808:instance-profile/eks-bastion-role
+
+aws configure set aws_access_key_id $ACCESS_KEY
+aws configure set aws_secret_access_key $SECRET_KEY
+aws ec2 associate-iam-instance-profile --instance-id $INSTANCE_ID --iam-instance-profile Arn=$INSTANCE_PROFILE
+#set kubeconfig to point to cluster
+aws eks update-kubeconfig --region us-gov-west-1 --name $CLUSTER_NAME
+#create back up of config map
+kubectl get cm aws-auth -n kube-system -o yaml > aws-auth.yaml
+##
+cat <<EOF> auth2.yaml
+apiVersion: v1
+  kind: ConfigMap
+  metadata:
+  name: aws-auth
+  namespace: kube-system
+  data:
+  #update mapRoles
+  mapRoles: | 
+    - rolearn: '${NodeInstanceRole.Arn}'
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+    - groups:
+        - system:masters
+        rolearn: arn:aws-us-gov:iam::713211132808:role/eks-bastion-role #same role that is attached to ec2-instance
+        username: eks-bastion-role
+EOF
+kubectl apply -f auth2.yaml
--- private-eks/cf-dev/ec2-userdata.sh
+++ private-eks/cf-dev/ec2-userdata.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 yum update -y
-yum install nano firewalld awscli -y
+yum install nano firewalld unzip iptables-services -y #will have to manually curl and unzip awscli on non aws linux distros
 systemctl enable firewalld
 systemctl start firewalld
 firewall-cmd --permanent --add-port={80,443,8850}/tcp
 firewall-cmd --reload
-firewall-cmd --list-all
+#downloads kubectl v1.29
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+#set aws credentials
+#export PATH=/usr/local/bin:$PATH
+source ~/.bash_profile
+if ! command -v aws &> /dev/null
+then
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  ./aws/install --update
+fi
 
