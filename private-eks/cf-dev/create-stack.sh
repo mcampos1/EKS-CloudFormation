2a502b3b8d064f07f0304692ff591cd1b6a9015f
markmccormack91 <mark.mccormack@mantech.com>
2024-04-15 10:39:59 -0600

    cleanup

--- private-eks/cf-dev/create-stack.sh
+++
@@ -1,6 +0,0 @@
-aws cloudformation create-stack \
-  --profile default \
-  --region us-gov-west-1 \
-  --stack-name test-cluster-stack \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --template-body file://cluster-stack.yaml 
