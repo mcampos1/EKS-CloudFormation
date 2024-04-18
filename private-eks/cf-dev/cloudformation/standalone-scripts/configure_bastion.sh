7254ed86ff408fff85a6fdd4d5914ce47cbad8b6
markmccormack91 <mark.mccormack@mantech.com>
2024-04-18 10:44:55 -0600

    update configure-bastion.sh

--- private-eks/cf-dev/cloudformation/standalone-scripts/configure_bastion.sh
+++ private-eks/cf-dev/cloudformation/standalone-scripts/configure_bastion.sh
@@ -4,14 +4,6 @@
 yum update -y
 yum install nano unzip iptables-services -y #will have to manually curl and unzip awscli on non aws linux distros
 
-sudo systemctl enable firewalld
-sudo systemctl start firewalld
-sudo firewall-cmd --permanent --add-port=22/tcp
-sudo firewall-cmd --permanent --add-port=80/tcp
-sudo firewall-cmd --permanent --add-port=443/tcp
-sudo firewall-cmd --permanent --add-port=8850/tcp
-sudo firewall-cmd --reload
-
 # Install kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
 chmod +x ./kubectl
@@ -27,12 +19,22 @@ then
   ./aws/install --update
 fi
 
-# Install eksctl
-# curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-# mv /tmp/eksctl /usr/bin
-
-# # Install helm
-# tar -zxvf helm-v3.0.0-linux-amd64.tar.gz
-
-# Update kubeconfig
+# # Update kubeconfig
 aws eks update-kubeconfig --name nested-cluster-mark --region us-gov-west-1
+
+# # Install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+mv /tmp/eksctl /usr/local/bin
+
+# Install helm
+curl https://github.com/helm/helm/releases/helm-v3.14.4-linux-amd64.tar.gz
+tar -zxvf helm-v3.0.0-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
+
+# sudo systemctl enable firewalld
+# sudo systemctl start firewalld
+# sudo firewall-cmd --permanent --add-port=22/tcp
+# sudo firewall-cmd --permanent --add-port=80/tcp
+# sudo firewall-cmd --permanent --add-port=443/tcp
+# sudo firewall-cmd --permanent --add-port=8850/tcp
+# sudo firewall-cmd --reload
