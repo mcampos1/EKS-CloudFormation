621067dc235cfbf049e6732f47b6cb9b28010e1d
markmccormack91 <mark.mccormack@mantech.com>
2024-04-18 15:14:26 -0600

    wip

--- private-eks/cf-dev/cloudformation/stacks-nested/cf-vars.sh
+++ private-eks/cf-dev/cloudformation/stacks-nested/cf-vars.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ## Stack Variables
-STACK_NAME="foundation-stack2"
+STACK_NAME="foundation-stack"
 
 ## VPC Variables ##
 REGION="us-gov-west-1"
 
 ## EKS Cluster Variables ##
-CLUSTER_NAME="nested-cluster-mark2"
+CLUSTER_NAME="nested-cluster-mark"
 CLUSTER_ROLE="arn:aws-us-gov:iam::713211132808:role/eksClusterRole"
 KEY_PAIR="martin-test"
 
@@ -19,9 +19,9 @@ NODEGROUP_AMI_ID="ami-068d2becc83de8e98"
 NODEGROUP_VOLUME_SIZE=100
 
 ## Bastion Host Variables ##
-BASTION_NAME="bastion-mark2"
+BASTION_NAME="bastion-mark"
 BASTION_ROLE="arn:aws-us-gov:iam::713211132808:role/eks-bastion-role"
 BASTION_INSTANCE_TYPE="t2.micro"
 # BASTION_AMI_ID="ami-04306748291e2183c"
 BASTION_AMI_ID="ami-0b09154de2f794b35"
-BASTION_SG_NAME="bastion-mark-sg2"
+BASTION_SG_NAME="bastion-mark-sg"
