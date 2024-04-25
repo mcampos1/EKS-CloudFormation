5be6970d4357b39de25fb08e8a614b25ff1e8b1c
jmarz20 <jazizmar20@gmail.com>
2024-04-25 16:44:09 -0500

    remove comment on name

--- private-eks/cf-dev/cloudformation/stacks-nested/cf-vars.sh
+++ private-eks/cf-dev/cloudformation/stacks-nested/cf-vars.sh
@@ -7,8 +7,8 @@ STACK_NAME="foundation-stack"
 REGION="us-gov-west-1"
 
 ## EKS Cluster Variables ##
-# CLUSTER_NAME="nested-cluster-mark"
-CLUSTER_NAME="nested-cluster-jaziz"
+CLUSTER_NAME="nested-cluster-mark"
+# CLUSTER_NAME="nested-cluster-jaziz"
 CLUSTER_ROLE="arn:aws-us-gov:iam::713211132808:role/eksClusterRole"
 KEY_PAIR="martin-test"
 
@@ -20,8 +20,8 @@ NODEGROUP_AMI_ID="ami-068d2becc83de8e98"
 NODEGROUP_VOLUME_SIZE=100
 
 ## Bastion Host Variables ##
-# BASTION_NAME="bastion-mark"
-BASTION_NAME="bastion-jaziz"
+BASTION_NAME="bastion-mark"
+# BASTION_NAME="bastion-jaziz"
 # BASTION_ROLE="arn:aws-us-gov:iam::713211132808:role/eks-bastion-role"
 BASTION_INSTANCE_TYPE="t2.micro"
 # BASTION_AMI_ID="ami-04306748291e2183c"
